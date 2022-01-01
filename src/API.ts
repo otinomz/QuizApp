86e1a126fee40cd6c6660adeb62a73f811cfea11
otinomz <awesomeotinomz@gmail.com>
2022-01-01 17:06:15 +0100

    reading api data

--- src/API.ts
+++ src/API.ts
@@ -1,6 +1,13 @@
 // logic for fetching data from API
 
-
+export type Question = {
+    category: string
+    difficulty: string
+    correct_answer: string
+    incorrect_answers: string[]
+    question: string
+    type: string
+}
 
 export enum Difficulty {
     EASY = "easy",
@@ -12,4 +19,4 @@ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json()
     console.log(data);   
-}  
+}
