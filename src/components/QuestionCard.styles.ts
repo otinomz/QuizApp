7e208e64396a652eff19c41fc79949bbf30e1558
otinomz <awesomeotinomz@gmail.com>
2022-01-02 14:56:01 +0100

    working on responsiveness

--- src/components/QuestionCard.styles.ts
+++ src/components/QuestionCard.styles.ts
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   max-width: 1100px;
+  width: 80%;
+  margin: 0 auto;
   background: #ebfeff;
   border-radius: 10px;
-  border: 2px solid #0085a3;
   padding: 20px;
   box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
   text-align: center;
@@ -20,15 +21,20 @@ type ButtonWrapperProps = {
 
 export const ButtonWrapper = styled.div<ButtonWrapperProps>`
   transition: all 0.3s ease;
+  width: 70%;
+  background: red;
+  margin: 0 auto;
+
   :hover {
     opacity: 0.8;
   }
+  
   button {
     cursor: pointer;
     user-select: none;
     font-size: 0.8rem;
     width: 100%;
-    height: 40px;
+    padding: 10px;
     margin: 5px 0;
     background: ${({ correct, userClicked }) =>
       correct
